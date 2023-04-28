cc8683efbe9cf628d6f3c2c34477ca19d96d208a
Devesh Sharma <devesh.sharma@srijan.net>
2023-04-28 23:42:25 +0530

    Added Terraform code

--- backed.tf
+++ backed.tf
@@ -2,8 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terrform-bucket-s3"
     key    = "env/prod"
-    access_key = "AKIAT52UCTQEJSUPFBWD" // add your key here
-    secret_key = "kaVdMMCWG2//c5Aj7eRT0qBEwHDG5GBJ9HVWYa1N" //// add your key here
     region = "us-east-1"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-    access_key = "AKIAT52UCTQEJSUPFBWD" // add your key here
-    secret_key = "kaVdMMCWG2//c5Aj7eRT0qBEwHDG5GBJ9HVWYa1N" //// add your key here
     region = "us-east-1"
 }
